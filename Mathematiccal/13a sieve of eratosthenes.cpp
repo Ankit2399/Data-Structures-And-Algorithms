ff1405fbb47dad0b68aa75dce6c4bce0642e1f16
ANKIT <67652706+Ankit2399@users.noreply.github.com>
2021-10-02 11:04:48 +0530

    File 1

---
+++ Mathematiccal/13a sieve of eratosthenes.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sieve(int n)//time comp. O(n*logn)
+{
+	vector<bool> isPrime(n + 1, true);
+
+	
+}
+
+int main()
+{
+	int n1 = 10, n2 = 100;
+
+	sieve(n1);
+	cout << endl;
+
+	sieve(n2);
+	cout << endl;
+
+	return 0;
+}
