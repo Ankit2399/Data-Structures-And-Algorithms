c05dae3eb128417ec192d6786fbf395676c271d6
Kartik Vij <92333419+KartikVij234@users.noreply.github.com>
2021-10-23 22:59:08 +0530

    Update 08 Merge Function of Merge Sort.cpp

--- Merging & Sorting/08 Merge Function of Merge Sort.cpp
+++ Merging & Sorting/08 Merge Function of Merge Sort.cpp
@@ -8,6 +8,34 @@ void merge(int a[], int low, int mid, int high) //time comp. O(n) ; space comp. 
 
 	int left[n1];
 	int right[n2];
+	
+	for (int i = 0; i < n1; i++)
+	{
+		left[i] = a[low + i];
+	}
+
+	for (int i = 0; i < n2; i++)
+	{
+		right[i] = a[mid + 1 + i];
+	}
+
+	int i = 0, j = 0, k = low;
+
+	while (i < n1 and j < n2)
+	{
+		if (left[i] <= right[j])
+		{
+			a[k] = left[i];
+			k++;
+			i++;
+		}
+		else
+		{
+			a[k] = right[j];
+			k++;
+			j++;
+		}
+	}
 
 	while (i < n1)
 	{
