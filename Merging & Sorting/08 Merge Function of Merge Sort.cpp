7edd7bfcd69b890020fab6a2376c2678146c9228
Kartik Vij <92333419+KartikVij234@users.noreply.github.com>
2023-10-01 12:55:08 +0530

    Update 08 Merge Function of Merge Sort.cpp

--- Merging & Sorting/08 Merge Function of Merge Sort.cpp
+++ Merging & Sorting/08 Merge Function of Merge Sort.cpp
@@ -1,71 +1,83 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-void merge(int a[], int low, int mid, int high) //time comp. O(n) ; space comp. O(n)
-{
-	int n1 = mid - low + 1;
-	int n2 = high - mid;
+/**
+ * @brief Merge two sorted subarrays into one sorted array.
+ *
+ * @param arr Reference to the array to be sorted.
+ * @param low Index of the first element of the first subarray.
+ * @param mid Index of the last element of the first subarray.
+ * @param high Index of the last element of the second subarray.
+ */
+void merge(vector<int>& arr, int low, int mid, int high) {
+    int n1 = mid - low + 1;
+    int n2 = high - mid;
 
-	int left[n1];
-	int right[n2];
-	
-	for (int i = 0; i < n1; i++)
-	{
-		left[i] = a[low + i];
-	}
+    // Create temporary vectors for the left and right subarrays
+    vector<int> left(n1);
+    vector<int> right(n2);
 
-	for (int i = 0; i < n2; i++)
-	{
-		right[i] = a[mid + 1 + i];
-	}
+    // Copy data to temporary vectors left[] and right[]
+    for (int i = 0; i < n1; i++) {
+        left[i] = arr[low + i];
+    }
 
-	int i = 0, j = 0, k = low;
+    for (int i = 0; i < n2; i++) {
+        right[i] = arr[mid + 1 + i];
+    }
 
-	while (i < n1 and j < n2)
-	{
-		if (left[i] <= right[j])
-		{
-			a[k] = left[i];
-			k++;
-			i++;
-		}
-		else
-		{
-			a[k] = right[j];
-			k++;
-			j++;
-		}
-	}
+    // Merge the two subarrays back into the original array
+    int i = 0, j = 0, k = low;
+    while (i < n1 && j < n2) {
+        if (left[i] <= right[j]) {
+            arr[k++] = left[i++];
+        } else {
+            arr[k++] = right[j++];
+        }
+    }
 
-	while (i < n1)
-	{
-		a[k] = left[i];
-		k++;
-		i++;
-	}
+    // Copy any remaining elements from the left and right subarrays
+    while (i < n1) {
+        arr[k++] = left[i++];
+    }
 
-	while (j < n2)
-	{
-		a[k] = right[j];
-		k++;
-		j++;
-	}
+    while (j < n2) {
+        arr[k++] = right[j++];
+    }
 }
 
-int main()
-{
-	int a[] = {1, 3, 5, 7, 2, 4, 6};
-	int n = sizeof(a) / sizeof(int);
+/**
+ * @brief Perform merge sort on an array.
+ *
+ * @param arr Reference to the array to be sorted.
+ * @param low Index of the first element of the array.
+ * @param high Index of the last element of the array.
+ */
+void mergeSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int mid = low + (high - low) / 2;
 
-	int low = 0;
-	int high = (n - 1);
-	int mid = (low + high) / 2;
+        // Recursively sort the first and second halves
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
 
-	merge(a, low, mid, high);
+        // Merge the sorted halves
+        merge(arr, low, mid, high);
+    }
+}
 
-	for (int i = 0; i < n; i++)
-	{
-		cout << a[i] << " ";
-	}
-	return 0;
+int main() {
+    vector<int> inputArray = {1, 3, 5, 7, 2, 4, 6};
+    int n = inputArray.size();
+
+    // Sort the inputArray using merge sort
+    mergeSort(inputArray, 0, n - 1);
+
+    // Print the sorted array
+    for (int i = 0; i < n; i++) {
+        cout << inputArray[i] << " ";
+    }
+    return 0;
 }
